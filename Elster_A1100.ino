0a24b4f0b505e3525a4a551fc465d06aa6164674
Jason Louie <jason.louie89@gmail.com>
2015-09-15 17:49:25 +1000

    Added support to 8266 wifi, using software RTC still

--- Elster_A1100.ino
+++ Elster_A1100.ino
@@ -9,55 +9,25 @@
   http://www.rotwang.co.uk/projects/meter.html
   Thanks Dave.
 */
+
+
+
+#include <Wire.h>
+#include "RTClib.h"
+#include <SoftwareSerial.h>
+#include <stdlib.h>
+
+RTC_Millis rtc;
+
 const uint8_t intPin = 2;
 #define BIT_PERIOD 860 // us
 #define BUFF_SIZE 64
-volatile long data[BUFF_SIZE];
-volatile uint8_t in;
-volatile uint8_t out;
-volatile unsigned long last_us;
-uint8_t dbug = 0;
-
-ISR(INT0_vect) {
-   unsigned long us = micros();
-   unsigned long diff = us - last_us;
-   if (diff > 20 ) {
-      last_us = us;
-      int next = in + 1;
-      if (next >= BUFF_SIZE) next = 0;
-      data[in] = diff;
-      in = next;
-   }
-}
-
-void setup() {
-    //pinMode(intPin, INPUT);
-    in = out = 0; 
-    Serial.begin(115200);
-    EICRA |= 3;    //RISING interrupt
-    EIMSK |= 1;    
-    if (dbug) Serial.print("Start ........\r\n");
-    last_us = micros();
-}
-
 unsigned int statusFlag;
-float imports;
-float exports;
-
-void loop() {
-//    decode_buff();
-  int rd = decode_buff();
-  if (!rd) return;
-  if (rd==3) {
-   Serial.println("");
-   Serial.print(imports);    Serial.print("\t");
-   Serial.print(exports);    Serial.print("\t");
-   Serial.print(statusFlag); Serial.println("");    
-  }
-//   unsigned long end_time = millis() + 6000;
-//   while (end_time >= millis()) ;
-}
-
+float imports=0;
+float exports=0;
+float transmittedImports=0;
+float transmittedExports=0;
+uint8_t transmittedsFlag=0;
 float last_data;
 uint8_t sFlag;
 float imps;
@@ -70,13 +40,127 @@ uint8_t pSum = 0;
 uint16_t BCC = 0;
 uint8_t eom = 1;
 
-static int decode_buff(void) {
+// connect 10 to TX of Serial USB
+// connect 11 to RX of serial USB
+SoftwareSerial ser(8, 9); // RX, TX
+
+// replace with your channel's thingspeak API key
+String apiKey = "G9GVCHD9LTQY6ZPO";
+
+volatile long data[BUFF_SIZE];
+volatile uint8_t in;
+volatile uint8_t out;
+volatile unsigned long last_us;
+uint8_t dbug = 0;
+
+void setup() {
+    //pinMode(intPin, INPUT);
+    in = out = 0; 
+    // enable software serial
+    Serial.begin(9600);
+    // enable Wifi serial
+    ser.begin(9600);
+    // reset ESP8266
+    ser.println("AT+RST");
+    Serial.println("8266 wifi initialised");
+    EICRA |= 3;    //RISING interrupt
+    EIMSK |= 1;    
+    if (dbug) Serial.print("Start ........\r\n");
+    last_us = micros();
+
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+}
+
+void loop() {
+//    decode_buff();
+ DateTime now = rtc.now();
+  int rd = decode_buff();
+     
+  if (!rd) return;
+  if (rd==3) 
+  {
+    //anonoymous readings are detected, do not know why
+    //need to eliminate and tidy up code
+    if (imports>=0 && exports>=0)
+    {
+       if (imports>exports)
+       { 
+          Serial.println("Transmitted Data Recorded");
+          Serial.print(transmittedImports);    Serial.print(",");
+          Serial.print(transmittedExports);    Serial.print(",");
+          Serial.print(transmittedsFlag); Serial.println("");
+          Serial.print(now.day(), DEC); 
+          Serial.print('/');
+          Serial.print(now.month(), DEC);
+          Serial.print('/');
+          Serial.print(now.year(), DEC);
+          Serial.print(",");
+          Serial.print(now.hour(), DEC);
+          Serial.print(':');
+          Serial.print(now.minute(), DEC);
+          Serial.print(':');
+          Serial.print(now.second(), DEC);
+          Serial.print(',');
+          Serial.print(imports);    Serial.print(",");
+          Serial.print(exports);    Serial.print(",");
+          Serial.print(statusFlag); Serial.println("");
+
+          //only transmit if the status Flag is a 1 or 0, if not do not transmit
+          //and check further.
+          if (statusFlag==1 || statusFlag==0)
+          {
+            //if transmitted imports and transmitted exports are blank no history 
+            //transmit data and then set transmitted prefix variables data to 
+            //data transmitted.
+            if (transmittedImports==0 && transmittedExports==0 && transmittedsFlag==0)
+            {
+                send8266Data(imports,exports,statusFlag);
+                transmittedImports=imports;
+                transmittedExports=exports;
+                transmittedsFlag=statusFlag;
+            }
+            //check to see if imports and exports are within range of maximum values.
+            
+            else if (imports<=(transmittedImports+70) && exports<=(transmittedExports+70))
+            {
+               //check to see if data is greater than historic data saved
+               if (imports>=transmittedImports && exports>=transmittedExports)
+               {
+                  send8266Data(imports,exports,statusFlag);
+                  transmittedImports=imports;
+                  transmittedExports=exports;
+                  transmittedsFlag=statusFlag;
+               }
+            }
+            else
+            {
+              Serial.println("Above IR received results not transmitted, erratic and invalid!!!");
+            }
+          }
+          else
+          {
+            Serial.println("Above IR received results not transmitted, erratic and invalid!!!");
+          }
+         }
+    }   
+  }
+//   unsigned long end_time = millis() + 6000;
+//   while (end_time >= millis()) ;
+}
+
+static int decode_buff(void) 
+{
    if (in == out) return 0;
    int next = out + 1;
    if (next >= BUFF_SIZE) next = 0;
    int p = (((data[out]) + (BIT_PERIOD/2)) / BIT_PERIOD);
    if (dbug) { Serial.print(data[out]); Serial.print(" "); if (p>500) Serial.println("<-"); }   
-   if (p>500) {
+   if (p>500) 
+   {
      idx = BCC = eom = imps = exps = sFlag = 0;   
      out = next;
      return 0;
@@ -84,12 +168,14 @@ static int decode_buff(void) {
    bit_left = (4 - (pSum % 5));
    bit_shft = (bit_left<p)? bit_left : p;
    pSum = (pSum==10)? p : ((pSum+p>10)? 10: pSum + p);
-   if (eom==2 && pSum>=7) {
+   if (eom==2 && pSum>=7)
+   {
       pSum=pSum==7?11:10;
       eom=0;   
    }
 
-   if (bit_shft>0) {
+   if (bit_shft>0) 
+   {
       byt_msg >>= bit_shft;
       if (p==2) byt_msg += 0x40<<(p-bit_shft);
       if (p==3) byt_msg += 0x60<<(p-bit_shft);
@@ -133,10 +219,75 @@ static int decode_buff(void) {
              Serial.print(pSum); Serial.print("\t");              
              Serial.print(byt_msg>>(pSum==10?1:2),BIN); Serial.print("\t"); //BCC read
              Serial.print((~BCC)&0x7F,BIN); Serial.print("\t"); //BCC calculated
-
           }
        }  
     }
     out = next;
     return 0;
 }
+
+ISR(INT0_vect) {
+   unsigned long us = micros();
+   unsigned long diff = us - last_us;
+   if (diff > 20 ) {
+      last_us = us;
+      int next = in + 1;
+      if (next >= BUFF_SIZE) next = 0;
+      data[in] = diff;
+      in = next;
+   }
+}
+
+static void send8266Data(float importData,float exportData, unsigned int statusFlag) 
+{
+   //Function to send data to thingspeak via 8266 with AT commands  
+   // TCP connection
+  String cmd = "AT+CIPSTART=\"TCP\",\"";
+ 
+  cmd += "144.212.80.11"; // api.thingspeak.com
+  cmd += "\",80";
+  Serial.println(cmd);
+  ser.println(cmd);
+   
+  if(ser.find("Error")){
+    Serial.println("AT+CIPSTART error");
+   return;
+  }
+
+  // prepare GET string
+  String getStr = "GET /update?api_key=";
+  getStr += apiKey;
+  getStr +="&field1=";
+  getStr +=importData;
+  getStr +="&field2=";
+  getStr +=exportData;
+  getStr +="&field3=";
+  getStr +=statusFlag;
+  getStr += "\r\n\r\n";
+  Serial.print(getStr); Serial.println("");
+  // send data length
+  if (getStr.length()==79)
+  {
+     cmd = "AT+CIPSEND=";
+     cmd += String(getStr.length());
+     Serial.print(cmd); Serial.println("");
+     ser.println(cmd);
+     if(ser.find(">"))
+     {
+        ser.print(getStr);
+     }
+     else
+     {
+        ser.println("AT+CIPCLOSE");
+        // alert user
+        Serial.println("AT+CIPCLOSE");
+       // thingspeak needs 15 sec delay between updates
+       delay(16000); 
+     }   
+   }
+   else
+   {
+      Serial.println("Did not send to thingspeak due to error in data");
+   }  
+}
+
